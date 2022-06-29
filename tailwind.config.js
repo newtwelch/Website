7f2d9aa9c7f7a0308497ae7cd9265f1d166ed216
Newt Welch <77295065+newtwelch@users.noreply.github.com>
2022-06-30 00:19:57 +0800

    Initial Commit 2

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'darkbg': '#05050B',
+        'lightbg': '#040517',
+        'primary': '#3747CD',
+        'secondary': '#FADFA5'
+      },
+      fontFamily:{
+        secular: ['Secular One', "sans-serif"],
+        unica: ['Unica One', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
