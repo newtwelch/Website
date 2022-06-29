78a176225cd4746ab3bc1dfeba5b351044a1a42f
ChristianKern97 <ikerneii97@gmail.com>
2022-06-29 21:45:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// HTTPS is handled by reverse proxy
+// app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
